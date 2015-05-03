7dcc21e3f84d103e37a8ab0f4715d80bbb0b1b18
Sudhir Khanger <sudhir@sudhirkhanger.com>
2015-05-03 17:09:23 +0530

    Lesson 2 complete

--- Sunshine/app/src/main/java/com/sudhirkhanger/app/sunshine/DetailedActivity.java
+++ Sunshine/app/src/main/java/com/sudhirkhanger/app/sunshine/DetailedActivity.java
@@ -1,20 +1,24 @@
 package com.sudhirkhanger.app.sunshine;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class MainActivity extends ActionBarActivity {
+public class DetailedActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_detailed);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new ForecastFragment())
+                    .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
     }
@@ -23,7 +27,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_detailed, menu);
         return true;
     }
 
@@ -42,4 +46,19 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detailed, container, false);
+            return rootView;
+        }
+    }
 }
