3d02594b0037f563914c0d5a1d5f795a7840d652
Sudhir Khanger <sudhir@sudhirkhanger.com>
2015-05-05 00:46:03 +0530

    Clicking on listview items can now open DetailedActivity and show forecast string + formatting

--- Sunshine/app/src/main/java/com/sudhirkhanger/app/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/com/sudhirkhanger/app/sunshine/ForecastFragment.java
@@ -105,7 +105,7 @@ public class ForecastFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String forecast = mForecastAdapter.getItem(position);
-                Intent intent = new Intent(getActivity(), DetailedActivity.class);
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
                 intent.putExtra(Intent.EXTRA_TEXT, forecast);
                 startActivity(intent);
             }
