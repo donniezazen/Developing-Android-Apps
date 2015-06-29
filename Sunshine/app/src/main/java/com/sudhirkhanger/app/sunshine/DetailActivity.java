bc51b91490a8e79d025dfd956c789e98f7e0d444
Sudhir Khanger <sudhir@sudhirkhanger.com>
2015-06-29 08:20:19 +0530

    5.09 two pane layout

--- Sunshine/app/src/main/java/com/sudhirkhanger/app/sunshine/DetailActivity.java
+++ Sunshine/app/src/main/java/com/sudhirkhanger/app/sunshine/DetailActivity.java
@@ -14,10 +14,10 @@ public class DetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detailed);
+        setContentView(R.layout.activity_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new DetailFragment())
+                    .add(R.id.weather_detail_container, new DetailFragment())
                     .commit();
         }
     }
